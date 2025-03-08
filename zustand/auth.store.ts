232194dd2ecc3e1dc0bbadffc036af89937a000f
quanghien <nquanghien97@gmail.com>
2025-03-08 08:30:56 +0700

    fix permission hocs

--- zustand/auth.store.ts
+++ zustand/auth.store.ts
@@ -8,20 +8,27 @@ interface AuthStoreType {
   me: UserEntity | null
   getMe: () => Promise<void>
   setMe: (user: UserEntity | ((prev: UserEntity | null) => UserEntity) | null) => void
+  isLoading: boolean
 }
 
 export const useAuthStore = create<AuthStoreType>()((set) => ({
   me: null,
+  isLoading: true,
   getMe: async () => {
     try {
       const token = Cookies.get('token');
+      if (!token) {
+        set({ isLoading: false });
+        return;
+      }
       const dataParse = await verifyToken(token || '')
-      if(dataParse?.user_id) {
+      if (dataParse?.user_id) {
         const res = await getMe()
-        set({ me: res.user })
+        set({ me: res.user, isLoading: false })
       }
     } catch (err) {
       console.log(err)
+      set({ isLoading: false })
     }
   },
   setMe: (item) =>
